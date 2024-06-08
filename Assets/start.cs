954211c2769f78a1057d4bb8c85248543ad741f6
Hüseyin Küçükşahin <hksahin06@gmail.com>
2024-06-09 00:58:16 +0300

    start

---
+++ Assets/start.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class scr : MonoBehaviour, IPointerClickHandler
+{
+    public string sceneName;
+
+    // Tıklama olayını yöneten metod
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Tıklandı");
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
